ea0d43072225fb744858b2dfe068a10fc3740b34
Triveni Gudidoddi <trivenigudidoddi@Trivenis-MacBook-Pro.local>
2023-12-30 02:46:59 -0500

    first commit

--- src/components/Experience/experience.js
+++ src/components/Experience/experience.js
@@ -20,7 +20,7 @@ const Experience = () => {
                     <br />
                     <h4>Software Engineer II</h4>    
                     <h5>Dec 2021 - April 2023</h5>   
-                    <p class="experience__description">Working on a robotic system to automate and reduce human efforts. Dealing with Machine Learning, Python APIs, Linux and Vision based systems</p>             
+                    <p class="experience__description">Worked on various products across the organisation in web and mobile applications encompassing both front-end and back-end development. Leveraging expertise in React, JavaScript, TypeScript, and Node, I contributed to the creation of seamless user interfaces and robust functionalities. </p>             
                 </div>
                 <div className="experience">
                     <h3>Oracle India Pvt. Ltd.</h3>
@@ -28,19 +28,9 @@ const Experience = () => {
                     <img className="experience__imagelink" src={OracleLogo} height="64" width="64" alt="Vested logo" />
                     <br />
                     <br />
-                    <h4>Software Engineer II</h4>    
-                    <h5>Dec 2021 - April 2023</h5>   
-                    <p class="experience__description">Working on a robotic system to automate and reduce human efforts. Dealing with Machine Learning, Python APIs, Linux and Vision based systems</p>             
-                </div>
-                <div className="experience">
-                    <h3>IIIT Hyderabad</h3>
-                    <br />
-                    <img className="experience__imagelink" src={IIITHyd} height="64" width="64" alt="Vested logo" />
-                    <br />
-                    <br />
-                    <h4>Research Intern</h4>    
-                    <h5>Dec 2021 - April 2023</h5>   
-                    <p class="experience__description">Working on a robotic system to automate and reduce human efforts. Dealing with Machine Learning, Python APIs, Linux and Vision based systems</p>             
+                    <h4>Member of Technical Staff</h4>    
+                    <h5>Sep 2020 - Dec 2021</h5>   
+                    <p class="experience__description">Worked on Oracle Autonomous cloud database monitoring and backup across 24+ country databases. Generated automated scripts and alerts using python, shell scripting and terraform.</p>             
                 </div>
                 <div className="experience">
                     <h3>Relevel</h3>
@@ -49,8 +39,18 @@ const Experience = () => {
                     <br />
                     <br />
                     <h4>Subject Matter Expert</h4>    
-                    <h5>Dec 2021 - April 2023</h5>   
-                    <p class="experience__description">Working on a robotic system to automate and reduce human efforts. Dealing with Machine Learning, Python APIs, Linux and Vision based systems</p>             
+                    <h5>June 2020 - Aug 2020</h5>   
+                    <p class="experience__description">Worked as SME for javascript and React classes and responsible for creating class plans, notes and projects for students. </p>  
+                </div>
+                <div className="experience">
+                    <h3>IIIT Hyderabad</h3>
+                    <br />
+                    <img className="experience__imagelink" src={IIITHyd} height="64" width="64" alt="Vested logo" />
+                    <br />
+                    <br />
+                    <h4>Research Intern</h4>    
+                    <h5>May 2019 - June 2019</h5>   
+                    <p class="experience__description">Worked as a Research Intern under Prof. Suryakanth V. Gangashetty, building ML/NLP models for Text-to-Speech systhesis in regional languages.</p>             
                 </div>
             </div>
         </div>
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -5,7 +5,7 @@ import '../../App.css'
 const Header = () => {
     const [showMenu, setShowMenu] = useState(false);
     return (
-        <>
+        <div>
             <div className='header center'>
             <h3 className='initials'>
                 <a href="/" >TG.</a>
@@ -25,13 +25,13 @@ const Header = () => {
         {showMenu && <div className='nav-mobile'>
             <svg onClick={() => setShowMenu(!showMenu)} className="cross-btn" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>
                 <ul>
-                    <li>education</li>
-                    <li>experience</li>
-                    <li>skills</li>
-                    <li>contact</li>
+                    <li onClick={() => setShowMenu(!showMenu)}><a href="#education">education</a></li>
+                    <li onClick={() => setShowMenu(!showMenu)}><a href="#experience">experience</a></li>
+                    <li onClick={() => setShowMenu(!showMenu)}><a href="#skills">skills</a></li>
+                    <li onClick={() => setShowMenu(!showMenu)}><a href="#contact">contact</a></li>
                 </ul>
             </div>}
-        </>
+        </div>
     )
 }
 
